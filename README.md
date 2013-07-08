e5838592ae24ed5dc87ae9cb4c29debd5270a604
Jose Mancera <jose.mancera@epfl.ch>
2013-07-08 16:44:08 +0200

    Version 3

---
+++ README.md
@@ -0,0 +1,3 @@
+Java Servlet Filter Example in Eclipse
+
+Still under construction, the instructions will be released when the project is finished
--- src/myfilter/MyFilter.java
+++ src/myfilter/MyFilter.java
@@ -1,4 +1,5 @@
 package myfilter;
+
 import java.io.IOException;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -24,36 +25,73 @@ public class MyFilter implements javax.servlet.Filter {
         servletContext = filterConfig.getServletContext();
         redirectPage = filterConfig.getInitParameter("Redirect-Page");
         log = Logger.getLogger(MyFilter.class.getName());
+        System.out.println("init method of filter"+"\n");
     }
 
-    public void doFilter(   ServletRequest req, 
-                            ServletResponse res, 
-                            FilterChain filterChain)
-        throws IOException, ServletException {
-
-        HttpServletRequest httpReq    = (HttpServletRequest)req;
-        HttpServletResponse    httpRes   = (HttpServletResponse)res;
+    public void doFilter(   ServletRequest req, ServletResponse res, FilterChain filterChain) throws IOException, ServletException {
+    	
+    	
+    	
+    	System.out.println("Arguments for the Do filter"+"\n");
+    	System.out.println("req: "+req);
+    	System.out.println("res: "+res);
+    	System.out.println("filterChain: "+filterChain+"\n");
+    	
+    	System.out.println("Do Filter method inside"+"\n");
+    	
+        HttpServletRequest httpReq   = (HttpServletRequest)req;
+        HttpServletResponse httpRes  = (HttpServletResponse)res;
+        
+        System.out.println("Http request: "+httpReq);
+        System.out.println("http response: "+httpRes);
+        
         
         HttpSession session = httpReq.getSession();
+                
+        System.out.println("session is: "+session+"\n");
+        System.out.println("Give me the address" +httpReq.getRemoteAddr());
+        System.out.println("GET PAth info "+httpReq.getPathInfo());
+        System.out.println("GET PAth Trans "+httpReq.getPathTranslated());
+        System.out.println("GET Query String "+httpReq.getQueryString());
+        System.out.println("GET Query String "+httpReq.getQueryString());
+        System.out.println("GET URI "+httpReq.getRequestURI());
+        System.out.println("GET URL "+httpReq.getRequestURL());
+        System.out.println("GET SERVLET PATH " +httpReq.getServletPath()+"\n");
+        
+        System.out.println("GET HEADER NAMES "+httpReq.getHeaderNames());
 
-        if(httpReq.getParameterValues("redirectme")!=null){
+        //TODO: We have to get a secret key generated randomly and compare it and not static 
+        if(httpReq.getParameterValues("secretkey:ELCA")!=null){
             httpRes.sendRedirect(redirectPage);
+            
+            
+            System.out.println("GRANTING ACCESS TO ELCA " + httpRes+"\n");
         }        
         else{
-            PrintWriter out = httpRes.getWriter();
-            CharResponseWrapper wrapper = new CharResponseWrapper(httpRes);
+        	System.out.println("no right password in the ELSE"+"\n");//
+        	PrintWriter out = httpRes.getWriter();
+            CharResponseWrapper wrapper = new CharResponseWrapper(httpRes);//
 
             filterChain.doFilter(httpReq, wrapper);
             
+            System.out.println("The content type is "+wrapper.getContentType());
+                        
+            System.out.println("if(wrapper.getContentType() != null && wrapper.getContentType().indexOf(text/html)!=-1)"+"\n");
+            
+            
             if(wrapper.getContentType() != null && wrapper.getContentType().indexOf("text/html")!=-1) {
+            	System.out.println("Inside of IF to write in index.html file");
                 CharArrayWriter caw = new CharArrayWriter();
                 caw.write(wrapper.toString().substring(0, wrapper.toString().indexOf("</body>")-1));
-                caw.write("<p>\nHello There</p>");
+                caw.write("<h1>You are not Authorized to access</h1>");
                 caw.write("\n</body></html>");
                 httpRes.setContentLength(caw.toString().length());
                 out.write(caw.toString());
             }
             else{
+            	
+            	System.out.println("skip writting index.html file");
+            	
                 out.write(wrapper.toString());
             }
             out.close();
